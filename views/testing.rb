18dca9fdb481e8f0badd608a9e54bf746689862b
Stephen Delaney <swdelaney2@gmail.com>
2015-12-08 15:21:10 -0600

    testing.rb contains functional give_training_week_number function

---
+++ views/testing.rb
@@ -0,0 +1,81 @@
+# @days_until_formatted = -40
+
+
+  def give_training_week_number(argument)
+    @inc = 1
+    @training_week_number
+  if (-7 < argument && argument < 0) || argument == 0
+    return @training_week_number = 1
+    elsif
+      until (-7 * @inc < argument && argument < -7 * (@inc-1)) || argument == -7 * (@inc-1) do
+        @inc += 1
+         @training_week_number = @inc
+      end
+
+      end
+      end
+
+give_training_week_number(-100)
+p @training_week_number
+
+
+
+#
+#
+# # LA LA LA
+# def give_training_week_number
+# if -7 < @days_until_formatted <= 0
+# return @training_week_number = 1
+# elsif
+# -7(2) < @days_until_formatted <= -7(2-1)
+# return @training_week_number = 2
+# elsif
+# -7(3) < @days_until_formatted <= -7(3-1)
+# return @training_week_number = 3
+# elsif
+# -7(4) < @days_until_formatted <= -7(4-1)
+# return @training_week_number = 4
+# elsif
+# -7(5) < @days_until_formatted <= -7(5-1)
+# return @training_week_number = 5
+# elsif
+# -7(6) < @days_until_formatted <= -7(6-1)
+# return @training_week_number = 6
+# elsif
+# -7(7) < @days_until_formatted <= -7(7-1)
+# return @training_week_number = 7
+# elsif
+# -7(8) < @days_until_formatted <= -7(8-1)
+# return @training_week_number = 8
+# elsif
+# -7(9) < @days_until_formatted <= -7(9-1)
+# return @training_week_number = 9
+# elsif
+# -7(10) < @days_until_formatted <= -7(10-1)
+# return @training_week_number = 10
+# elsif
+# -7(11) < @days_until_formatted <= -7(11-1)
+# return @training_week_number = 11
+# elsif
+# -7(12) < @days_until_formatted <= -7(12-1)
+# return @training_week_number = 12
+# elsif
+# -7(13) < @days_until_formatted <= -7(13-1)
+# return @training_week_number = 13
+# elsif
+# -7(14) < @days_until_formatted <= -7(14-1)
+# return @training_week_number = 14
+# elsif
+# -7(15) < @days_until_formatted <= -7(15-1)
+# return @training_week_number = 15
+# elsif
+# -7(16) < @days_until_formatted <= -7(16-1)
+# return @training_week_number = 16
+# elsif
+# -7(17) < @days_until_formatted <= -7(17-1)
+# return @training_week_number = 17
+# elsif
+# -7(18) < @days_until_formatted <= -7(18-1)
+# return @training_week_number = 18
+# end
+# end
