3e25f746f910fff0cb1b45ab5ea7ebc2abf27f56
Stephen Delaney <swdelaney2@gmail.com>
2015-12-09 09:58:54 -0600

    cleaned up tracker controller, set functions apart

--- controllers/tracker.rb
+++ controllers/tracker.rb
@@ -2,28 +2,48 @@ class TrackerController < ApplicationController
 
 require 'date'
 
-  get '/dashboard' do
-
-    provide_user_id
-    provide_user_name
-    @user_name
-    @user_id
-@current_marathon_name = Setting.find_by(account_id: @user_id).marathon_name
-@current_unformatted_marathon_date = Setting.find_by(account_id: @user_id).marathon_date
-
-@current_date = Date.today
-@current_week = @current_date.strftime("%W")
-
+def create_novice
+@novice = Hash.new
+@novice[1] = ['Rest', '3 miles', '3 miles', '3 miles', 'Rest', '6 miles', 'Cross']
+@novice[2] = ['Rest', '3 miles', '3 miles', '3 miles', 'Rest', '7 miles', 'Cross']
+@novice[3] = ['Rest', '3 miles', '4 miles', '3 miles', 'Rest', '5 miles', 'Cross']
+@novice[4] = ['Rest', '3 miles', '4 miles', '3 miles', 'Rest', '9 miles', 'Cross']
+@novice[5] = ['Rest', '3 miles', '5 miles', '3 miles', 'Rest', '10 miles', 'Cross']
+@novice[6] = ['Rest', '3 miles', '5 miles', '3 miles', 'Rest', '7 miles', 'Cross']
+@novice[7] = ['Rest', '3 miles', '6 miles', '3 miles', 'Rest', '12 miles', 'Cross']
+@novice[8] = ['Rest', '3 miles', '6 miles', '3 miles', 'Rest', '13 miles', 'Cross']
+@novice[9] = ['Rest', '3 miles', '7 miles', '4 miles', 'Rest', '10 miles', 'Cross']
+@novice[10] = ['Rest', '3 miles', '7 miles', '4 miles', 'Rest', '15 miles', 'Cross']
+@novice[11] = ['Rest', '4 miles', '8 miles', '4 miles', 'Rest', '16 miles', 'Cross']
+@novice[12] = ['Rest', '4 miles', '8 miles', '5 miles', 'Rest', '12 miles', 'Cross']
+@novice[13] = ['Rest', '4 miles', '9 miles', '5 miles', 'Rest', '18 miles', 'Cross']
+@novice[14] = ['Rest', '5 miles', '9 miles', '5 miles', 'Rest', '14 miles', 'Cross']
+@novice[15] = ['Rest', '5 miles', '10 miles', '5 miles', 'Rest', '20 miles', 'Cross']
+@novice[16] = ['Rest', '5 miles', '8 miles', '4 miles', 'Rest', '12 miles', 'Cross']
+@novice[17] = ['Rest', '4 miles', '6 miles', '3 miles', 'Rest', '8 miles', 'Cross']
+@novice[18] = ['Rest', '3 miles', '4 miles', '2 miles', 'Rest', '26 miles', 'Cross']
+end
 
 def format_date(date)
    @fixed_date = date.gsub('-',',')
  @new_array = @fixed_date.split(',')
 @current_formatted_marathon_date = DateTime.new(@new_array[0].to_i, @new_array[1].to_i, @new_array[2].to_i)
 end
-format_date(@current_unformatted_marathon_date)
 
-@trainingStart = @current_formatted_marathon_date - 125
-@week_one = @trainingStart.strftime("%W")
+def provide_marathon_info
+  @current_marathon_name = Setting.find_by(account_id: @user_id).marathon_name
+  @current_unformatted_marathon_date = Setting.find_by(account_id: @user_id).marathon_date
+end
+
+def set_date
+  @current_date = Date.today
+  @current_week = @current_date.strftime("%W")
+end
+
+def set_training_start
+  @trainingStart = @current_formatted_marathon_date - 125
+  @week_one = @trainingStart.strftime("%W")
+end
 
 def how_many_weeks_away
 if @week_one.to_i - @current_week.to_i < 0
@@ -34,12 +54,11 @@ else
   return @weeks_away = 'Zero'
 end
 end
-how_many_weeks_away
 
-
-
-@days_until = @trainingStart - @current_date
-@days_until_formatted = @days_until.to_i
+def set_days_until
+  @days_until = @trainingStart - @current_date
+  @days_until_formatted = @days_until.to_i
+end
 
 def give_training_week_number(argument)
   @inc = 1
@@ -53,52 +72,43 @@ elsif (-7 < argument && argument < 0) || argument == 0
       @inc += 1
        @training_week_number = @inc
     end
-
     end
     end
 
-give_training_week_number(@days_until_formatted)
+    def tracker_total_distance
+    provide_user_id
+    @current_marathon_settings_id = Setting.find_by(account_id: @user_id).id
+    @select_tracker = Tracker.where(settings_id: @current_marathon_settings_id)
 
-def tracker_total_distance
-provide_user_id
-@current_marathon_settings_id = Setting.find_by(account_id: @user_id).id
-@select_tracker = Tracker.where(settings_id: @current_marathon_settings_id)
+    # Currenttly, calling select_tracker.to_json.... need to figure out how to add everything up
 
-# Currenttly, calling select_tracker.to_json.... need to figure out how to add everything up
+    # return @select_tracker
+    # if select_tracker
+    # @total_distance = select_tracker.distance_mon.to_i + select_tracker.distance_tues.to_i + select_tracker.distance_wed.to_i + select_tracker.distance_thurs.to_i + select_tracker.distance_fri.to_i + select_tracker.distance_sat.to_i + select_tracker.distance_sun.to_i
+    # return @total_distance
+    # else
+    #   return @total_distance = 0
+    # end
+    end
 
-# return @select_tracker
-# if select_tracker
-# @total_distance = select_tracker.distance_mon.to_i + select_tracker.distance_tues.to_i + select_tracker.distance_wed.to_i + select_tracker.distance_thurs.to_i + select_tracker.distance_fri.to_i + select_tracker.distance_sat.to_i + select_tracker.distance_sun.to_i
-# return @total_distance
-# else
-#   return @total_distance = 0
-# end
-end
-tracker_total_distance
+  get '/dashboard' do
 
-    @novice = Hash.new
-    @novice[1] = ['Rest', '3 miles', '3 miles', '3 miles', 'Rest', '6 miles', 'Cross']
-    @novice[2] = ['Rest', '3 miles', '3 miles', '3 miles', 'Rest', '7 miles', 'Cross']
-    @novice[3] = ['Rest', '3 miles', '4 miles', '3 miles', 'Rest', '5 miles', 'Cross']
-    @novice[4] = ['Rest', '3 miles', '4 miles', '3 miles', 'Rest', '9 miles', 'Cross']
-    @novice[5] = ['Rest', '3 miles', '5 miles', '3 miles', 'Rest', '10 miles', 'Cross']
-    @novice[6] = ['Rest', '3 miles', '5 miles', '3 miles', 'Rest', '7 miles', 'Cross']
-    @novice[7] = ['Rest', '3 miles', '6 miles', '3 miles', 'Rest', '12 miles', 'Cross']
-    @novice[8] = ['Rest', '3 miles', '6 miles', '3 miles', 'Rest', '13 miles', 'Cross']
-    @novice[9] = ['Rest', '3 miles', '7 miles', '4 miles', 'Rest', '10 miles', 'Cross']
-    @novice[10] = ['Rest', '3 miles', '7 miles', '4 miles', 'Rest', '15 miles', 'Cross']
-    @novice[11] = ['Rest', '4 miles', '8 miles', '4 miles', 'Rest', '16 miles', 'Cross']
-    @novice[12] = ['Rest', '4 miles', '8 miles', '5 miles', 'Rest', '12 miles', 'Cross']
-    @novice[13] = ['Rest', '4 miles', '9 miles', '5 miles', 'Rest', '18 miles', 'Cross']
-    @novice[14] = ['Rest', '5 miles', '9 miles', '5 miles', 'Rest', '14 miles', 'Cross']
-    @novice[15] = ['Rest', '5 miles', '10 miles', '5 miles', 'Rest', '20 miles', 'Cross']
-    @novice[16] = ['Rest', '5 miles', '8 miles', '4 miles', 'Rest', '12 miles', 'Cross']
-    @novice[17] = ['Rest', '4 miles', '6 miles', '3 miles', 'Rest', '8 miles', 'Cross']
-    @novice[18] = ['Rest', '3 miles', '4 miles', '2 miles', 'Rest', '26 miles', 'Cross']
+    provide_user_id
+    provide_user_name
+    create_novice
+    provide_marathon_info
+    set_date
+    format_date(@current_unformatted_marathon_date)
+    set_training_start
+    how_many_weeks_away
+    set_days_until
+    give_training_week_number(@days_until_formatted)
+    tracker_total_distance
+
     erb :dashboard
 
   end
-  # ^ end of get dashboard?
+  # ^ end of get dashboard
 
 
 
@@ -139,6 +149,20 @@ else
 end
   end
 
-
+  get '/dashboard_week' do
+    provide_user_id
+    provide_user_name
+    create_novice
+    provide_marathon_info
+    set_date
+    format_date(@current_unformatted_marathon_date)
+    set_training_start
+    how_many_weeks_away
+    set_days_until
+    give_training_week_number(@days_until_formatted)
+    tracker_total_distance
+    
+    erb :dashboard_week
+  end
 
 end
