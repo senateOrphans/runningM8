b5a54c0cdb55a417b32ef7c702483fdad33f0db5
Aaron <akr.git.receiver@gmail.com>
2015-12-10 14:27:21 -0600

    functioning user info + style

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -41,6 +41,8 @@ $(document).ready(function(){
   //end of password match
   //~~~~~~~~~~~~~
 
+  //~~~~~~~~~~~~~
+  //start of tglr
   $( ".tglr" ).click(function(e) {
     if( 0 == tracker % 2){
       $( "#header_logo" ).fadeTo("slow", 1);
@@ -62,6 +64,47 @@ $(document).ready(function(){
       console.log(tracker);
     }
   });
+  //end of tglr
+  //~~~~~~~~~~~~~
+  //~~~~~~~~~~~~~
+  //user info container animations
+  $("#expand_container").click(function(){
+    $('#login_info_container').css(
+      'display', 'block'
+    )
+    $('#login_info_container').animate({
+      width: "100%",
+      opacity: '1'
+    },1000, function(){
+      $('#login_info').animate({
+        opacity: '1'
+      }, 500, function(){
+        $('#close_container').animate({
+          opacity: '1'
+        }, 500)
+      })
+    });
+  });
 
-
+  //close_container
+  $('#close_container').click(function(){
+    $('#close_container').animate({
+      opacity: '0'
+    }, 500, function(){
+      $('#login_info').animate({
+        opacity: '0'
+      }, 500, function(){
+      $('#login_info_container').animate({
+        opacity: '0',
+        width: "0"
+      },1000, function(){
+        $('#login_info_container').css(
+          'display', 'none'
+        );
+      });
+    });
+  });
+  });
+  //end of animations
+  //~~~~~~~~~~~~~
 });
