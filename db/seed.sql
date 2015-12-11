5e257dc8506d53e5b6c89a1cbee4211de816cf9a
Aaron <akr.git.receiver@gmail.com>
2015-12-11 11:06:26 -0600

    style changes + seed file changes

--- db/seed.sql
+++ db/seed.sql
@@ -1,14 +1,5 @@
 INSERT INTO accounts (user_name, user_email, password_digest, join_date) VALUES ('Stephen21', 'swdelaney2@gmail.com', 'password', CURRENT_DATE);
 
--- Week 1
-INSERT INTO novice (monday, tuesday, wednesday, thursday, friday, saturday, sunday) VALUES ('Rest', '3 miles', '3 miles', '3 miles', 'Rest', '6', 'Cross');
-
--- Week 2
-INSERT INTO novice (monday, tuesday, wednesday, thursday, friday, saturday, sunday) VALUES ('Rest', '3 miles', '3 miles', '3 miles', 'Rest', '7', 'Cross');
-
--- Week 3
-INSERT INTO novice (monday, tuesday, wednesday, thursday, friday, saturday, sunday) VALUES ('Rest', '3 miles', '4 miles', '3 miles', 'Rest', '5', 'Cross');
-
 -- Week 4
 -- Week 5
 -- Week 6
